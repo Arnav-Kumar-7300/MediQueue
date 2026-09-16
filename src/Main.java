8ede760cb263592c1eee748a9624207864f66305
Arnav Kumar <karnav7300@gmail.com>
2026-09-16 18:53:09 +0530

    Initial MediQueue project setup

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("==========================================================");
+        System.out.println("🏥 MediQueue");
+        System.out.println("Hospital Appointment & Intelligent Queue Management System");
+        System.out.println("==========================================================");
+
+        System.out.println();
+        System.out.println("Welcome to MediQueue!");
+    }
+}
